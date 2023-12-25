7f700e1f5f89a4baa2dc1b2fb522751026c1e01e
dsreitan <plagfinn@gmail.com>
2023-12-25 18:52:40 +0100

    Puppeteer set cache path in prod

---
+++ .puppeteerrc.cjs
@@ -0,0 +1,9 @@
+const { join } = require("path");
+/**
+ * https://azureossd.github.io/2023/05/05/Running-Puppeteer-on-Azure-App-Service-Linux/
+ * @type {import("puppeteer").Configuration}
+ */
+module.exports = {
+  // Changes the cache location for Puppeteer.
+  cacheDirectory: process.env.NODE_ENV === "production" ? join(__dirname, ".cache", "puppeteer") : undefined,
+};
