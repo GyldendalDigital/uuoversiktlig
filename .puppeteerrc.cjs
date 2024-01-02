2976288c507ef38c0256e57c7978e9662efda57a
dsreitan <plagfinn@gmail.com>
2024-01-02 08:56:29 +0100

    Update .gitignore and .puppeteerrc.cjs, add postinstall script in package.json

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -5,5 +5,5 @@ const { join } = require("path");
  */
 module.exports = {
   // Changes the cache location for Puppeteer.
-  cacheDirectory: process.env.NODE_ENV === "production" ? join(__dirname, ".cache", "puppeteer") : undefined,
+  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
 };
